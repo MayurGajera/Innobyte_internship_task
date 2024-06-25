526a67df16c37d1f1b5944e0ae9f3bed814f779d
Mayur <mayurgajera135@gmail.com>
2024-06-25 19:45:46 +0530

    Task cmpleted

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = () => {
     };
     // Attempt to connect to MongoDB using environment variable URL
     try {
-        mongoose.connect(process.env.URL, connectionParams); // Connect to MongoDB using URL from environment variables
+        mongoose.connect(process.env.MONGODB_URL, connectionParams); // Connect to MongoDB using URL from environment variables
         console.log("Connected to database successfully")
     } catch (error) {
         console.log(error)
